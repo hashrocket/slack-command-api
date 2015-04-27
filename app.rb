14ed7eb4327b57d0426f5eea0e7b32ed4c58c14b
Joshua Plicque <plicjo@gmail.com>
2015-04-27 15:40:24 -0400

    Update message text

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ require 'bundler'
 Bundler.require
 
 post '/late' do
-   params['text'] = "Hey team, #{params[:user_name]} is gonna be late. This team member will be in around #{params[:text].strip}."
+   params['text'] = "Hey team, #{params[:user_name]} is gonna be late. He/She will be in around #{params[:text].strip}."
    RestClient.post(ENV['SLACK_POST_URL'], params.to_json)
 end
-
 
