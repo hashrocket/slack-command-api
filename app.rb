67d57130f795f27be649dd1aabe6c4e9a51c833d
Joshua Plicque <plicjo@gmail.com>
2015-04-27 14:18:24 -0400

    Personalize slack message

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require
 
 post '/late' do
-   params['text'] = "Hey team, I'm gonna be late. I'll be in around #{params[:text].strip}."
+   params['text'] = "Hey team, #{params.user_name} is gonna be late. This team member will be in around #{params[:text].strip}."
    RestClient.post(ENV['SLACK_POST_URL'], params.to_json)
 end
 
