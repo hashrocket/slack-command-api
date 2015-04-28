48938123d8f4e1140885a9b167a8a1f27dd6472b
Chad Brading and Joshua Plicque <dev+chadbrading+plicjo@hashrocket.com>
2015-04-28 11:49:14 -0400

    Optionally post the late command to a different channel

--- command_processor.rb
+++ command_processor.rb
@@ -5,6 +5,20 @@ class CommandProcessor
    end
 
    def process_late_command
-      {'text' => "Hey team, #{@user_name} is gonna be late. He/She will be in around #{@text}."}
+      if @text.include?('#')
+         {'text' => "Hey team, #{@user_name} is gonna be late. He/She will be in around #{process_text}.", 'channel' => process_channel}
+      else
+         {'text' => "Hey team, #{@user_name} is gonna be late. He/She will be in around #{@text}."}
+      end
    end
+
+   private
+
+     def process_text
+        @text.split('#')[0].strip
+     end
+
+     def process_channel
+        @text[@text.index('#')..-1]
+     end
 end
--- spec/command_processor_spec.rb
+++ spec/command_processor_spec.rb
@@ -2,9 +2,34 @@ require_relative 'spec_helper'
 
 describe 'Command Processor' do
 
+  def app
+    Sinatra::Application
+  end
+
+  def data
+    {
+        token:        'KjRUKVRBoQVerm6bJTymvOe0',
+        team_id:      'T0001',
+        team_domain:  'example',
+        channel_id:   'C2147483705',
+        channel_name: 'other_channel',
+        user_id:      'U2147483697',
+        user_name:    'Steve',
+        command:      '/late',
+        text:         '10AM'
+    }
+  end
+
   it 'processes a late command' do
     output = CommandProcessor.new(data).process_late_command
     expect(output).to eq({'text' => 'Hey team, Steve is gonna be late. He/She will be in around 10AM.'})
+  end
+
+  it 'optionally posts to a custom channel' do
+    channel_data = data
+    channel_data[:text] = '10AM #other_channel'
+    output = CommandProcessor.new(channel_data).process_late_command
+    expect(output).to eq({'text' => 'Hey team, Steve is gonna be late. He/She will be in around 10AM.', 'channel' => '#other_channel'})
   end
 
 end
